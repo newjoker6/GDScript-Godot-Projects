8f5ee9316d8df336f647791b7f3c5b69b9bdc8e9
Michael McGuire <newjoker6@gmail.com>
2022-05-07 10:28:06 -0700

    Work Order System Added

---
+++ Work Order System/WorkOrderSystem.gd
@@ -0,0 +1,40 @@
+extends Control
+
+
+var API_KEY = 'qkvZbz1nYAFRNbk0AOHM6qAvohFkcPc3'
+var url_base = 'http://www.mapquestapi.com/directions/v2/route'
+var city = 'Surrey+BC+CA'
+var from_location = ''
+var to_location = ''
+var url = '%s?key=%s&from=%s+%s&to=%s+%s' %[url_base, API_KEY, from_location, city, to_location, city]
+
+
+func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+	var response = parse_json(body.get_string_from_utf8())
+	list_directions(response['route']['legs'][0]['maneuvers'])
+
+
+func list_directions(directions):
+	for d in directions:
+		if d['narrative']:
+			$Directions.add_text(str(d['narrative']))
+			$Directions.newline()
+
+
+func _on_DirectionButton_pressed():
+	format_search()
+	$Directions.clear()
+	url = '%s?key=%s&from=%s+%s&to=%s+%s' %[url_base, API_KEY, from_location, city, to_location, city]
+	$HTTPRequest.request(url)
+
+
+func format_search():
+	for c in $ToLocation.text:
+		if c == " ":
+			c = '+'
+		to_location += c
+		
+	for c in $FromLocation.text:
+		if c == " ":
+			c = '+'
+		from_location += c
